fe65f2af72102975e651bcd93682fb95a67212e7
Valentina_Gonzalez <valentina.gonzalez06@uceva.edu.co>
2026-09-11 16:46:57 -0500

    test(molecules): pruebas unitarias de FormFieldMolecule

--- projects/design-system-bootstrap/src/lib/presentation/molecules/form-field/form-field.molecule.spec.ts
+++ projects/design-system-bootstrap/src/lib/presentation/molecules/form-field/form-field.molecule.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { FormFieldMolecule } from './form-field.molecule';
+import { LabelAtom } from '../../atoms/label/label.atom';
+import { InputAtom } from '../../atoms/input/input.atom';
 
 describe('FormFieldMolecule', () => {
   let component: FormFieldMolecule;
@@ -8,16 +10,52 @@ describe('FormFieldMolecule', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FormFieldMolecule]
-    })
-    .compileComponents();
+      imports: [FormFieldMolecule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FormFieldMolecule);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Debería crear el componente', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Debería pasar las propiedades label, idField y required al LabelAtom', () => {
+    component.label = 'Correo electrónico';
+    component.idField = 'user-email';
+    component.required = true;
+    fixture.detectChanges();
+
+    const labelDebugEl = fixture.debugElement.query(By.directive(LabelAtom));
+    expect(labelDebugEl).toBeTruthy();
+
+    const labelComponent = labelDebugEl.componentInstance as LabelAtom;
+    expect(labelComponent.text).toBe('Correo electrónico');
+    expect(labelComponent.idFor).toBe('user-email');
+    expect(labelComponent.required).toBe(true);
+  });
+
+  it('Debería pasar las propiedades idField y type al InputAtom', () => {
+    component.idField = 'user-email';
+    component.type = 'email';
+    fixture.detectChanges();
+
+    const inputDebugEl = fixture.debugElement.query(By.directive(InputAtom));
+    expect(inputDebugEl).toBeTruthy();
+
+    const inputComponent = inputDebugEl.componentInstance as InputAtom;
+    expect(inputComponent.idInput).toBe('user-email');
+    expect(inputComponent.type).toBe('email');
+  });
+
+  it('Debería emitir el evento valueChange cuando InputAtom emite un nuevo valor', () => {
+    const spy = jest.spyOn(component.valueChange, 'emit');
+    const inputDebugEl = fixture.debugElement.query(By.directive(InputAtom));
+    const inputComponent = inputDebugEl.componentInstance as InputAtom;
+
+    inputComponent.valueChange.emit('nuevo-valor');
+    expect(spy).toHaveBeenCalledWith('nuevo-valor');
   });
 });

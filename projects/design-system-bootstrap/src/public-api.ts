f3475a9aa1286bb8af7074ff26d4d5615f09b94e
Valentina_Gonzalez <valentina.gonzalez06@uceva.edu.co>
2026-09-11 16:46:29 -0500

    feat(molecules): export de FormFieldMolecule en public-api

--- projects/design-system-bootstrap/src/public-api.ts
+++ projects/design-system-bootstrap/src/public-api.ts
@@ -16,6 +16,7 @@ export { InputAtom } from './lib/presentation/atoms/input/input.atom';
 //Molecules
 export { NavLinkMolecule } from './lib/presentation/molecules/nav-link/nav-link.molecule';
 export { ButtonGroupMolecule } from './lib/presentation/molecules/button-group/button-group.molecule';
+export { FormFieldMolecule } from './lib/presentation/molecules/form-field/form-field.molecule';
 
 //Organisms
 export { NavbarOrganism } from './lib/presentation/organisms/navbar/navbar.organism';
